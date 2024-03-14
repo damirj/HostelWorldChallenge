8ce83e092a1787ecb656ce0a3e05f75a8ccdffe2
damirj <d.jurkovic934@gmail.com>
2024-03-14 18:38:13 +0100

    Add UI for PropertyDetailsFragment

--- app/src/main/java/com/example/hostelworldchallenge/network/models/RatingBreakdown.kt
+++ app/src/main/java/com/example/hostelworldchallenge/network/models/RatingBreakdown.kt
@@ -1,12 +1,9 @@
 package com.example.hostelworldchallenge.network.models
 
 data class RatingBreakdown(
-    val average: Int,
     val clean: Int,
     val facilities: Int,
-    val `fun`: Int,
     val location: Int,
-    val ratingsCount: Int,
     val security: Int,
     val staff: Int,
     val value: Int
